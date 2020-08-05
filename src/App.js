225a3aad77e4e9c281b7d26d16cf2d08c5226329
krjaladi <k.jaladi@gmail.com>
2020-08-05 16:36:38 +0200

    expermenting Route and Switch from react-router-dom API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import {Route,Switch} from 'react-router-dom';
+
+const Foo = () => (
+  <div>
+    <h2>Page 'foo'</h2>
+  </div>
+);
 
 
 function App() {
   return (
     <div>
-        <Homepage />
+      <Switch>
+        <Route  exact path = '/'>
+          <Homepage />
+        </Route>
+        <Route path = '/foo'>
+          <Foo />
+        </Route>
+      </Switch>
+      
+
     </div>
   );
 }
